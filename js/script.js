bbb2f388a7193f67a19707c43d7fbf5b7bfdf671
carlosvillaar <carlosvillar004@gmail.com>
2024-11-26 18:47:17 +0100

    feat: add comparison method

--- js/script.js
+++ js/script.js
@@ -4,13 +4,11 @@ const numOneRandom = document.getElementById('num-random-one');
 const numTwoRandom = document.getElementById('num-random-two');
 const numThreeRandom = document.getElementById('num-random-three');
 const numFourRandom = document.getElementById('num-random-four');
-const numOne = document.getElementById('num-one');
-const numTwo = document.getElementById('num-two');
-const numThree = document.getElementById('num-three');
-const numFour = document.getElementById('num-four');
+const input = document.querySelectorAll('input');
 const button = document.getElementById('button');
 const containerRandom = document.getElementById('container-random');
 const containerForm = document.getElementById('container-form')
+const alert = document.getElementById('alert');
 
 
 //Timer
@@ -27,16 +25,35 @@ const timer = setInterval(() => {
 
 //Aggiungo i numeri random all'HTML e li pusho nell'array
 const randomNumbers = [];
-randomNumbers.push(numOneRandom.innerHTML = randomNumber(1,50))
-randomNumbers.push(numTwoRandom.innerHTML = randomNumber(1,50))
-randomNumbers.push(numThreeRandom.innerHTML = randomNumber(1,50))
-randomNumbers.push(numFourRandom.innerHTML = randomNumber(1,50))
+randomNumbers.push(numOneRandom.innerHTML = randomNumber(1,50));
+randomNumbers.push(numTwoRandom.innerHTML = randomNumber(1,50));
+randomNumbers.push(numThreeRandom.innerHTML = randomNumber(1,50));
+randomNumbers.push(numFourRandom.innerHTML = randomNumber(1,50));
+
+button.addEventListener('click', () => {
+  const equalNumbers = [];
+  //leggo i valori degli input
+  for (i = 0; i < input.length; i++){
+    if (randomNumbers.includes(parseInt(input[i].value))){
+      equalNumbers.push(parseInt(input[i].value))
+    } 
+  }
+  if (equalNumbers.length === 0){
+    alert.innerHTML = `<p style="color: red; padding-top: 20px;">Alzheimer precoce? Non hai indovinato nessun numero. Riprova!</p>`
+  } else if (equalNumbers.length >= 2){
+    alert.innerHTML = `<p style="color: green; padding-top: 20px;">Bravissimo, hai indovinato ${equalNumbers.length} numeri. I numeri indovinati sono ${equalNumbers}.</p>`
+    
+  }else if ((equalNumbers.length = 1)) {
+    alert.innerHTML = `<p style="color: orange; padding-top: 20px;">Poteva andare meglio, hai indovinato un numero. Il numero indovinato è ${equalNumbers}.</p>`
+  }
+
+  containerRandom.classList.remove('hidden')
+  containerForm.classList.add('hidden')
+})
 
 
 
 //Funzioni
-
-//numero random
 function randomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
